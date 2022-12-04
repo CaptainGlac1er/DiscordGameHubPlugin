c349ace0883d81aa6b345c64e693d3b40f9d5ef1
George Walter Colgrove IV <georgecolgrove@outlook.com>
2022-12-04 10:10:15 -0500

    more updates

--- GameHubInteractionService.cs
+++ GameHubInteractionService.cs
@@ -4,8 +4,8 @@ using GlacierByte.Discord.Server.Api;
 
 namespace GlacierByte.Discord.Plugin;
 public class GameHubInteractionService : ICustomService {
-    private readonly DiscordSocketClient _client;
     private readonly LongTermFileService _fileService;
+    private readonly Dictionary<String, Func<SocketMessageComponent, Task>> _tempButtonResponse = new();
 
     private IDictionary<string, Game> _gamesAvailable;
 
@@ -15,8 +15,8 @@ public class GameHubInteractionService : ICustomService {
         )
     {
         _gamesAvailable = new Dictionary<string, Game>();
-        _client = client;
         client.ModalSubmitted += ModalHandler;
+        client.ButtonExecuted += ButtonHandler;
         _fileService = fileStorage;
     }
 
@@ -29,8 +29,24 @@ public class GameHubInteractionService : ICustomService {
         await context.Interaction.RespondWithModalAsync(newGameModal.Build());
     }
 
+    public void AddUserToGame(string game, SocketUser user)
+    {
+        this._gamesAvailable[game].addPlayerThatWantsToPlay(user.Id);
+    }
+    
+    public void RemoveUserToGame(string game, SocketUser user)
+    {
+        this._gamesAvailable[game].removePlayerThatWantsToPlay(user.Id);
+    }
+
     public List<string> GetGameNames() {
         return _gamesAvailable.Keys.ToList();
+    }
+
+    public Boolean IsUserInGame(string gameName, SocketUser user)
+    {
+        return _gamesAvailable.ContainsKey(gameName) &&
+               _gamesAvailable[gameName].PlayersThatWantToPlay.ContainsKey(user.Id);
     }
 
     public List<ulong> GetListOfPlayersThatPlayGame(string gameName) {
@@ -38,6 +54,35 @@ public class GameHubInteractionService : ICustomService {
             return new List<ulong>();
         }
         return _gamesAvailable[gameName].getListOfPlayersPlayingGame();
+    }
+    
+    private async Task ButtonHandler(SocketMessageComponent component)
+    {
+        if (this._tempButtonResponse.ContainsKey(component.Data.CustomId))
+        {
+            await this._tempButtonResponse[component.Data.CustomId].Invoke(component);
+            return;
+        }
+        
+        switch (component.Data.CustomId)
+        {
+            case "ADD_ME_IN":
+                await component.RespondAsync($"{component.User.Mention} has clicked the button", ephemeral: true);
+                break;
+            default:
+                await component.DeferAsync(ephemeral: true);
+                break;
+        }
+    }
+
+    public void addTempCommand(string id, Func<SocketMessageComponent, Task> action)
+    {
+        this._tempButtonResponse.Add(id, action);
+    }
+    
+    public void removeTempCommand(string id)
+    {
+        this._tempButtonResponse.Remove(id);
     }
 
     public async Task ModalHandler(SocketModal modal) {
@@ -49,7 +94,7 @@ public class GameHubInteractionService : ICustomService {
                 var gameName = newGameName.Value;
                 if(!_gamesAvailable.ContainsKey(gameName)) {
                     var newGame = new Game(gameName);
-                    newGame.addPlayerThatWantsToPlay(_client.CurrentUser.Id);
+                    newGame.addPlayerThatWantsToPlay(modal.User.Id);
                     _gamesAvailable.Add(gameName, newGame);
                 }
                 break;
--- GameHubPlugin.cs
+++ GameHubPlugin.cs
@@ -1,7 +1,9 @@
-﻿using Discord.Interactions;
+﻿using System.Text;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
 
 namespace GlacierByte.Discord.Plugin;
-    // [Group("gamehub")]
 public class GameHubPlugin : InteractionModuleBase<SocketInteractionContext>
 {
     private readonly GameHubInteractionService Service;
@@ -16,13 +18,13 @@ public class GameHubPlugin : InteractionModuleBase<SocketInteractionContext>
     public async Task GetGames() {
         
         var games = Service.GetGameNames();
-        var reply = "Available Games to Play\r\n";
+        var reply = new StringBuilder("Available Games to Play\r\n");
         foreach (var gameName in games)
         {
-            reply += $"{gameName}\r\n";
+            reply.AppendLine($"{gameName}");
         }
 
-        await RespondAsync(reply);
+        await RespondAsync(reply.ToString());
     }
     [SlashCommand("who_plays_this_game", "Get List of players that play a game")]
     public async Task GetPlayersThatPlayGame(
@@ -31,28 +33,100 @@ public class GameHubPlugin : InteractionModuleBase<SocketInteractionContext>
             Autocomplete(typeof(GameAutocompleteHandler))
         ] string gameName
         ) {
-        var players = Service.GetListOfPlayersThatPlayGame(gameName);
-        var playerString = $"Players playing {gameName}\r\n[";
-
-        foreach (var player in players) {
-            playerString += $"{player},\r\n";
-        }
-        playerString += "]";
-        await RespondAsync(playerString);
+            var embedBuilder = this._getPlayersThatPlayGame(gameName);
+            var replyCommandBuilder = this._getWhoPlaysGameButtons(gameName);
+            await RespondAsync(embed: embedBuilder.Build(), components: replyCommandBuilder.Build(), options: new RequestOptions(), ephemeral: true);
     }
+
+    [ComponentInteraction("addUserToGame:game(*)", ignoreGroupNames: true)]
+    public async Task AddUserToGame(string gameName)
+    {
+        Service.AddUserToGame(gameName, Context.User);
+        await Context.Interaction.ModifyOriginalResponseAsync(newComponent =>
+        {
+            var embedBuilder = this._getPlayersThatPlayGame(gameName);
+            newComponent.Embed = embedBuilder.Build();
+            newComponent.Components = this._getWhoPlaysGameButtons(gameName).Build();
+        });
+    }
+
+    [ComponentInteraction("removeUserFromGame:game(*)", ignoreGroupNames: true)]
+    public async Task RemoveUserFromGame(string gameName)
+    {
+        Service.RemoveUserToGame(gameName, Context.User);
+        await Context.Interaction.ModifyOriginalResponseAsync(newComponent =>
+        {
+            var embedBuilder = this._getPlayersThatPlayGame(gameName);
+            newComponent.Embed = embedBuilder.Build();
+            newComponent.Components = this._getWhoPlaysGameButtons(gameName).Build();
+        });
+    }
+    
+    
+    [ComponentInteraction("pingForGame(game:*)", ignoreGroupNames: true)]
+    public async Task PingForGame(string gameName)
+    {
+        var playerEmbed = _getPlayersThatPlayGame(gameName);
+        await Context.Interaction.DeleteOriginalResponseAsync();
+        await Context.Channel.SendMessageAsync(text: $"{Context.User.Mention} wants to play {gameName}", embed: playerEmbed.Build(),
+                                                           allowedMentions: AllowedMentions.All);
+    }
+
+    private EmbedBuilder _getPlayersThatPlayGame(string gameName)
+    {
+        var players = Service.GetListOfPlayersThatPlayGame(gameName);
+        var playerString = new StringBuilder($"Players playing {gameName}\r\n");
+
+        foreach (var player in players)
+        {
+            var user = Context.Guild.GetUser(player);
+            playerString.AppendLine($"{user.Mention},");
+        }
+
+        var embedBuilder = new EmbedBuilder();
+        embedBuilder.WithTitle(gameName);
+        embedBuilder.WithDescription(playerString.ToString());
+        return embedBuilder;
+    }
+
+    private ComponentBuilder _getWhoPlaysGameButtons(string gameName)
+    {
+        var replyCommandBuilder = new ComponentBuilder();
+        ActionRowBuilder rowBuilder = new ActionRowBuilder();
+        replyCommandBuilder.AddRow(rowBuilder);
+        rowBuilder.AddComponent(Service.IsUserInGame(gameName, Context.User)
+            ? _getRemoveMeButton(gameName).Build()
+            : _getAddMeButton(gameName).Build()
+        );
+        rowBuilder.AddComponent(new ButtonBuilder().WithLabel("Ping").WithStyle(ButtonStyle.Primary)
+            .WithCustomId($"pingForGame(game:{gameName})").Build());
+        return replyCommandBuilder;
+    }
+
+    private ButtonBuilder _getAddMeButton(string gameName)
+    {
+        return new ButtonBuilder().WithStyle(ButtonStyle.Primary).WithLabel("Add Me In").WithCustomId($"addUserToGame:game({gameName})");
+    }
+    
+    private ButtonBuilder _getRemoveMeButton(string gameName)
+    {
+        return new ButtonBuilder().WithStyle(ButtonStyle.Danger).WithLabel("Remove Me").WithCustomId($"removeUserFromGame:game({gameName})");
+    }
+    
     [SlashCommand("save_state", "Save state of Game Service")]
     public async Task SaveState() {
         try {
             await Service.SaveState();
-            await RespondAsync("State saved");
+            await RespondAsync("State saved", ephemeral: true);
         } catch (Exception e) {
-            await RespondAsync(e.Message);
+            await RespondAsync(e.Message, ephemeral: true);
         }
     }
+
     [SlashCommand("load_state", "Save state of Game Service")]
     public async Task LoadState() {
         await Service.LoadState();
-        await RespondAsync("State loaded");
+        await RespondAsync("State loaded", ephemeral: true);
     }
 }
 
